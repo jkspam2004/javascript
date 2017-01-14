ad612f3a55bf6ed8ff2b2c2bec5feb593af7971a
jkspam2004 <jkspam2004@yahoo.com>
2017-01-13 16:40:20 -0800

    more sort() examples

--- sort.js
+++ sort.js
@@ -28,3 +28,33 @@ function compare_desc(a, b) {
     return b - a;
 }
 console.log(arr.sort(compare_desc)); // [1, 2, 10, 21, 35]
+
+/* sort by values of specified key in an object */
+var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic', value: 13 },
+    { name: 'Zeros', value: 37 }
+];
+
+function sortByValue(a, b) {
+    return a.name - b.name;
+}
+console.log(items.sort(sortByValue));
+
+/*
+    String comparison
+
+    function compare(a, b) {
+        if (a is less than b by some ordering criterion) {
+            return -1;
+        }
+        if (a is greater than b by the ordering criterion) {
+            return 1;
+        }
+        // a must be equal to b
+        return 0;
+    }
+*/
